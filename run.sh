a649be64fb166fcce3b1a9f012a465ef8766c1bc
ClaudeSeo <ehdaudtj@gmail.com>
2020-02-11 18:52:17 +0900

    chore: run.sh 스크립트 변경

--- run.sh
+++ run.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
-
+docker pull localstack/localstack
 kubectl apply -f local-stack.yaml
-
 echo "Pod 가 준비될 때 까지 대기합니다..."
+kubectl wait --timeout=150s \
+    --for=condition=Ready \
+    pod/"$(kubectl get po --sort-by=.metadata.creationTimestamp | grep local-stack | grep -e "ContainerCreating" -e "Running" -e "Pending" | awk 'END{print $1}')"
 
-kubectl wait --for=condition=Ready pod/"$(kubectl get po --sort-by=.metadata.creationTimestamp | grep local-stack | grep -e "ContainerCreating" -e "Running" | awk 'END{print $1}')"
-
+./provisioning.sh
